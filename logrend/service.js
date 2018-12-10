2127ef4eff84095f0710f368a48f85eda71521b9
Steve Hillier <stephenhillier@gmail.com>
2018-12-09 17:07:03 -0800

    service handles own auth (via cookie)

--- logrend/service.js
+++ logrend/service.js
@@ -6,8 +6,10 @@ import http from 'http'
 const jwt = require('express-jwt');
 // const jwtAuthz = require('express-jwt-authz');
 const jwksRsa = require('jwks-rsa');
+const cookieParser = require('cookie-parser')
 
 const app = express();
+app.use(cookieParser())
 
 // Adapted from original code by Inventid https://github.com/inventid/pedeffy/blob/master/src/index.js
 // and https://github.com/diegomura/react-pdf/issues/269
@@ -42,6 +44,18 @@ const checkJwt = jwt({
     jwksRequestsPerMinute: 1,
     jwksUri: `https://earthworks.auth0.com/.well-known/jwks.json`
   }),
+
+
+  getToken: function fromHeaderOrQuerystring (req) {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+        return req.headers.authorization.split(' ')[1];
+    } else if (req.query && req.query.token) {
+      return req.query.token;
+    } else if (req.cookies && req.cookies.access_token) {
+      return req.cookies.access_token
+    }
+    return null;
+  },
 
   // Validate the audience and the issuer.
   audience: 'https://earthworks.islandcivil.com',
