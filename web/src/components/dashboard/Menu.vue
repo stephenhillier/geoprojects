a45aa411e977a68de719fd6d0b9d0e77243e8ac4
Steve <stephenhillier@gmail.com>
2019-03-22 18:42:02 -0700

    clean up old code

--- web/src/components/dashboard/Menu.vue
+++ web/src/components/dashboard/Menu.vue
@@ -1,22 +1,4 @@
 <template>
-  <!-- <b-card>
-    <nav>
-      <div>
-        <ul class="nav flex-column">
-          <b-nav-item class="menu-item" exact :to="{ name: 'projects' }"><font-awesome-icon class="text-muted mr-3" :icon="['fas', 'th-list']"></font-awesome-icon>Back to project list</b-nav-item>
-          <b-nav-item active-class="active-menu" class="menu-item mt-3" exact :to="{ name: 'project-dashboard', params: { id: this.$route.params.id } }">
-              Project: {{ project.name }}
-          </b-nav-item>
-          <b-nav-item active-class="active-menu" class="menu-item" :to="{ name: 'project-boreholes', params: { id: this.$route.params.id } }">
-            <font-awesome-icon :icon="['far', 'dot-circle']" class="text-muted mr-3"></font-awesome-icon>Boreholes<b-badge variant="primary" class="float-right mt-1" pill>{{ project.borehole_count }}</b-badge>
-          </b-nav-item>
-          <b-nav-item active-class="active-menu" class="menu-item" :to="{ name: 'lab-home', params: { id: this.$route.params.id } }">
-            <font-awesome-icon :icon="['far', 'clipboard']" class="text-muted mr-3"></font-awesome-icon>Lab tests<b-badge variant="primary" pill></b-badge>
-          </b-nav-item>
-        </ul>
-      </div>
-    </nav>
-  </b-card> -->
   <nav class="menu earthworks-project-menu">
     <p class="menu-label">
       Project: {{ project.name }}
