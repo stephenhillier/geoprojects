881c04dba12d8ca9cb22d9d858203e8fb64fe104
Steve Hillier <stephenhillier@gmail.com>
2019-03-28 22:36:03 -0700

    remvoe unused code

--- web/src/views/ProjectDashboard.vue
+++ web/src/views/ProjectDashboard.vue
@@ -1,29 +1,4 @@
 <template>
-  <!-- <div id="dashboard" class="mb-3">
-    <b-row v-if="!!project && project != {}">
-      <b-col cols="12" xl="2">
-        <project-menu :project="project" :project-id="project.id"></project-menu>
-      </b-col>
-      <b-col>
-        <b-row>
-          <b-col class="mb-3">
-            <b-card>
-              <div class="card-status bg-blue"></div>
-              <b-breadcrumb class="bg-light m-0 p-0 mb-3" :items="breadcrumbs"></b-breadcrumb>
-              <router-view :project="project" @update-project="fetchProjectData"></router-view>
-            </b-card>
-          </b-col>
-          <b-col cols="12" xl="2">
-            <div> -->
-              <!-- Actions -->
-              <!-- <router-view :project="project" name="actions"></router-view>
-            </div>
-          </b-col>
-        </b-row>
-      </b-col>
-
-    </b-row>
-  </div> -->
   <div id="dashboard">
     <div class="columns is-fullheight" v-if="!!project && project != {}">
       <div class="column is-narrow is-fullheight">
